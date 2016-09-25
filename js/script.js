d200199d7d2e49bb6e94e6b93d4b75f6ce83d7dd
Carolyn Lam <caro_lyn_@hotmail.com>
2016-09-24 21:10:08 -0400

    update stylesheet

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-var theSrc = "";
-var theId = "";
-var theSrc2 = "";
-var theId2 = "";
-var divId = "";
-var clickCount = 0;
 var score = 0;
+var pair = false;
+var cardNum = 0;
 var imagesArray = [
 
                     "images/pokemon1.png",
@@ -19,80 +15,74 @@ var imagesArray = [
                     "images/pokemon10.png"
                   ];
 
-function shuffle() {
-    newImgsArr = [];
-    newImgsArr = $.merge($.merge([], imagesArray), imagesArray);
-    var length = newImgsArr.length;
-    var currentImg = $("#images div:first-child");
-    for (var i = 0; i < length; i++) {
-        var randomNum = Math.round(Math.random() * (newImgsArr.length - 1));
-        $("#" + currentImg.attr("id") + " img").attr("src", newImgsArr[randomNum]);
-        newImgsArr.splice(randomNum, 1);
-        currentImg = currentImg.next();
-    }
+function makeCards() {
+     var newArr = [];
+     var count = 0;
+     while (count < 2) {
+          $.each(imagesArray, function(index, value) {
+               newArr.push(value);
+          });
+          count += 1;
+     }
+     shuffle(newArr);
 }
 
-var one = "";
-var two = "";
-var cardNum = 0;
-function play() {
-if (cardNum === 10) {
-    setTimeout(function(){
-        $("#message").html("You completed the game in " + score + " moves!");
-    }, 600);
-    // setTimeout(function(){
-    //     alert("AMAZING!! You completed the game in " + score + " moves!");
-    // }, 600);
-} else if (clickCount === 0) {
-    divId = $(this).attr("id");
-        if ($("#" + divId + " img").is(":hidden")) {
-            $("#" + divId + " img").show("slow");
-            one = $("#" + divId + " img");
-            theSrc = $("#" + divId + " img").attr("src");
-            clickCount++;
-        }
-    } else if (clickCount === 1) {
-        divId2 = $(this).attr("id");
-        if ($("#" + divId2 + " img").is(":hidden")) {
-            $("#" + divId2 + " img").show("slow");
-            two = $("#" + divId2 + " img");
-            theSrc2 = $("#" + divId2 + " img").attr("src");
-            clickCount++;
-            score++;
-            $("#moves").html(score);
-            if (theSrc !== theSrc2) {
-                setTimeout(function(){
-                    one.hide("slow");
-                    two.hide("slow");
-                    divId = "";
-                    divId2 = "";
-                }, 600);
-            } else {
-                cardNum++;
-            }
-            clickCount = 0;
-            play();
-        }
-    }
+function shuffle(arr) {
+     var length = arr.length;
+     for(var i = 0; i < length; i++) {
+          // random num between 0 - 19
+          var randomNum = Math.floor(Math.random() * arr.length);
+          $("#images").append("<div id='img" + i + "'><img src='" + arr[randomNum] +"'></div>");
+          arr.splice(randomNum, 1);
+     }
 }
 
 function resetGame() {
-    cardNum = 0;
-    clickCount = 0;
-    score = 0;
-    $("#message").html("");
-    $("#moves").html(score);
-    shuffle();
-    $("img").hide();
+     score = 0;
+     pair = false;
+     cardNum = 0;
+     $("#moves").html(score);
+     $("#images").children().remove();
+     makeCards();
+     $("#images div").on("click", play);
 }
 
-$(document).ready(function(){
-    for (var i = 0; i < 2; i++) {
-        $.each(imagesArray, function(index, value){
-            $("#images").append("<div id=img" + i + index + "><img src=" + value + "></div>");
-        });
-    }
-    shuffle();
-    $("#images div").click(play);
-    $("#reset").click(resetGame);
+function play() {
+     var currCardId = "#" + $(this).attr("id");
+     var currCard = $(currCardId + " img");
+     if (currCard.is(":hidden")) {
+          $(currCard).show("slow");
+          isMatch(currCard);
+     }
+}
+
+function isMatch(card) {
+     if (!(pair)) {
+          card1 = card;
+          pair = true;
+     } else {
+          card2 = card;
+          if (card1.attr("src") !== card2.attr("src")) {
+               setTimeout(function() {
+                    $(card1).hide("slow");
+                    $(card2).hide("slow");
+               }, 600);
+          } else {
+               cardNum++;
+          }
+          score++;
+          $("#moves").html(score);
+          pair = false;
+          if (cardNum === 10) {
+               setTimeout(function() {
+                    $("#message").html("Congratulations! You completed the game in " + score + " moves!");
+               }, 800);
+          }
+     }
+}
+
+$(document).ready(function() {
+     makeCards();
+     $("#images div").on("click", play);
+     $("#reset").on("click", resetGame);
 });
